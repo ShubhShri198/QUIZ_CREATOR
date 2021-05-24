33a304f6b041d74596f294b1acbc2707335e86c6
ShubhShri198 <63647698+ShubhShri198@users.noreply.github.com>
2021-05-24 19:50:40 +0530

    Add files via upload

---
+++ Userspecs.py
@@ -0,0 +1,9 @@
+class student:
+
+    def __init__(self,name,email):
+        self.name=name
+        self.email=email
+
+    def getdetails(self):
+        print('Name: ',self.name)
+        print('Email-id: ',self.email)
